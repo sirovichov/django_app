74792b2d3456e874d2864e11ce02e4855f439444
sirovichov <arskur1412@gmail.com>
2024-07-25 03:01:36 +0400

    fixes

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('~/prj')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))   
